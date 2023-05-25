9235da5a0356896ff60d797fae8c2d6d813abedc
Jatin Yadav <jatinyadav471@gmail.com>
2023-05-25 14:57:03 +0530

    Initial Commit

---
+++ iNews/iNewsApp.swift
@@ -0,0 +1,17 @@
+//
+//  iNewsApp.swift
+//  iNews
+//
+//  Created by JATIN YADAV on 25/05/23.
+//
+
+import SwiftUI
+
+@main
+struct iNewsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
